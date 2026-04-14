1722ce4b3356a56536b94fe5b4ee552ab40b5e7b
BlupBlurp <44441061+BlupBlurp@users.noreply.github.com>
2026-04-14 09:40:22 +0200

    Fix some lint issues

--- data/random-battles/gen8relumi/teams.ts
+++ data/random-battles/gen8relumi/teams.ts
@@ -2,10 +2,10 @@ import RandomTeams from "../gen9/teams";
 
 export class RandomRelumiTeams extends RandomTeams {
 	override randomSets: {
-		[species: string]: RandomTeamsTypes.RandomSpeciesData;
+		[species: string]: RandomTeamsTypes.RandomSpeciesData,
 	} = require("./sets.json");
 	override randomDoublesSets: {
-		[species: string]: RandomTeamsTypes.RandomSpeciesData;
+		[species: string]: RandomTeamsTypes.RandomSpeciesData,
 	} = require("./doubles-sets.json");
 }
 
