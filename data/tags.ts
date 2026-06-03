6a5fa4e3329a3188e65b7ec3fa5fbc0b5e5a708e
BlupBlurp <44441061+BlupBlurp@users.noreply.github.com>
2026-06-03 21:52:06 +0200

    Merge remote-tracking branch 'upstream/master'

--- data/tags.ts
+++ data/tags.ts
@@ -173,10 +173,7 @@ export const Tags: { [id: IDEntry]: TagData } = {
 	// -----
 	uber: {
 		name: "Uber",
-		speciesFilter: (species) =>
-			species.tier === "Uber" ||
-			species.tier === "(Uber)" ||
-			species.tier === "AG",
+		speciesFilter: species => species.tier === 'Uber' || species.tier === 'AG' || species.tier === '(AG)',
 	},
 	ou: {
 		name: "OU",
@@ -213,8 +210,7 @@ export const Tags: { [id: IDEntry]: TagData } = {
 	},
 	pu: {
 		name: "PU",
-		speciesFilter: (species) =>
-			species.tier === "PU" || species.tier === "(NU)",
+		speciesFilter: species => species.tier === 'PU',
 	},
 	zubl: {
 		name: "ZUBL",
@@ -222,8 +218,7 @@ export const Tags: { [id: IDEntry]: TagData } = {
 	},
 	zu: {
 		name: "ZU",
-		speciesFilter: (species) =>
-			species.tier === "(PU)" || species.tier === "ZU",
+		speciesFilter: species => species.tier === 'ZU',
 	},
 	nfe: {
 		name: "NFE",
@@ -247,7 +242,7 @@ export const Tags: { [id: IDEntry]: TagData } = {
 	},
 	ag: {
 		name: "AG",
-		speciesFilter: (species) => species.tier === "AG",
+		speciesFilter: species => species.tier === 'AG' || species.tier === '(AG)',
 	},
 
 	// Doubles tiers
@@ -279,12 +274,11 @@ export const Tags: { [id: IDEntry]: TagData } = {
 	// -------------
 	ndag: {
 		name: "ND AG",
-		speciesFilter: (species) => species.natDexTier === "AG",
+		speciesFilter: species => species.natDexTier === 'AG' || species.natDexTier === '(AG)',
 	},
 	nduber: {
 		name: "ND Uber",
-		speciesFilter: (species) =>
-			species.natDexTier === "Uber" || species.natDexTier === "(Uber)",
+		speciesFilter: species => species.natDexTier === 'Uber',
 	},
 	ndou: {
 		name: "ND OU",
